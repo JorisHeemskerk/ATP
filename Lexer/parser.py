109dc23acb22d8948036cfa18776fe7998413c02
Joris Heemskerk <joris.heemskerk@student.hu.nl>
2021-09-29 13:53:33 +0200

    missed certain files due to git add fucking me analy

--- Lexer/parser.py
+++
@@ -1,9 +0,0 @@
-from typing import List,  Tuple
-import tokens as tok
-import statements as stat
-
-# parser :: [Token] -> CodeBlock -> ([Token], CodeBlock)
-def parser(token_list : List[tok.Token], code : stat.CodeBlock) -> Tuple[List[tok.Token], stat.CodeBlock]:
-    
-    if len(token_list) == 0:
-        return None, code
